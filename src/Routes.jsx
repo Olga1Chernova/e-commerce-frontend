5400fb47067b155e8f62659f9ee4c9ed93408cd1
Olga1Chernova <107894331+Olga1Chernova@users.noreply.github.com>
2023-05-30 03:13:21 +0300

    add start files

---
+++ src/Routes.jsx
@@ -0,0 +1,8 @@
+import Login from '../src/pages/Login.jsx'
+const Routes = () => {
+  return (
+    <p>login</p>
+  )
+}
+
+export default Routes
---
+++ src/styles/styles.js
@@ -0,0 +1,20 @@
+const styles = {
+  custom_container: "w-11/12 sm:w-full",
+  heading: "text-3xl text-center md:text-left font-semibold font-Roboto pb-4 text-gray-900",
+  section: "w-11/12 mx-auto",
+  productTitle: "text-2xl font-semibold font-Roboto text-gray-800 hover:text-indigo-600 transition-colors duration-300",
+  productDiscountPrice: "text-lg font-bold text-gray-800",
+  price: "text-base text-gray-500 line-through",
+  shop_name: "text-sm text-blue-400 pb-2",
+  active_indicator: "absolute bottom-[-7px] left-0 h-2 w-full bg-crimson",
+  button:
+    "w-48 bg-black h-12 my-4 flex items-center justify-center rounded-xl cursor-pointer text-white font-semibold hover:bg-gray-900 transition-colors duration-300",
+  cart_button:
+    "px-8 h-10 rounded-full bg-fuchsia-500 flex items-center justify-center cursor-pointer hover:bg-fuchsia-600 transition-colors duration-300",
+  cart_button_text: "text-white text-lg font-semibold",
+  input: "w-full border border-gray-300 p-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-indigo-400",
+  activeStatus: "w-4 h-4 rounded-full absolute top-1 right-1 bg-green-500",
+  normalFlex: "flex items-center",
+};
+
+export default styles;
